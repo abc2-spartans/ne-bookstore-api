39248cfc7aea7d63cae4284799eab049c574282a
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2025-07-27 23:02:13 +0530

    feat: add sample endpoint for REST APIs

--- app.js
+++ app.js
@@ -22,8 +22,45 @@ app.get("/health", (req, res) => {
 // CRUD endpoints for books
 app.get("/api/v1/books", (req, res) => {
     console.log(req.query)
+    // Add read all books logic here
     res.status(200);
     res.json([]);
+});
+
+app.post("/api/v1/books", (req, res) => {
+    console.log(req.body)
+    const { title, author, published_year } = req.body;
+    if (!title || !author)
+        return res.status(400).json({ error: "Title and author required" });
+    // Add create logic here
+    res.status(201).json({ id: 1, title, author, published_year });
+});
+
+app.get("/api/v1/books/:id", (req, res) => {
+    console.log(req.params)
+    const id = req.params.id;
+    // Add read single book for given id logic here
+    const book = {
+        id,
+        title: "Book 1",
+        author: "Author 1",
+        published_year: 2020
+    }
+    res.status(201).json(book);
+});
+
+app.put("/api/v1/books/:id", (req, res) => {
+    const id = req.params.id;
+    const { title, author, published_year } = req.body;
+    // Add update logic here
+    res.status(204).json({ message: "Book updated successfully" });
+});
+
+
+app.delete("/api/v1/books/:id", (req, res) => {
+    const id = req.params.id;
+    // Add delete logic here
+    res.status(204).json({ message: "Book deleted successfully" });
 });
 
 app.listen(PORT, hostname, () => {
