c0c0a279f9491b4b85188829a491bbd5904464ce
Gaurav Singh <gauravsinghaec@gmail.com>
2025-07-28 23:15:10 +0530

    fix: db config path

--- src/db/init.js
+++ src/db/init.js
@@ -7,7 +7,7 @@ const config = getConfig();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dbPath = path.join(__dirname, '../../', config.sqlite.database);
+const dbPath = path.join(__dirname, "../../", config.database.sqlite.database);
 
 // Create and export the database connection
 export const db = new sqlite3.Database(dbPath, (err) => {
